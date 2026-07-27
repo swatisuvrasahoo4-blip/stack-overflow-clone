ad02ae105c36d9516161b8ffcfff7769eae8942b
swatisuvrasahoo4-blip <swatisuvrasahoo4@gmail.com>
2026-07-27 22:29:14 +0530

    fix: update community page

---
+++ stack/src/pages/community/create.tsx
@@ -0,0 +1,11 @@
+import Mainlayout from "@/layout/Mainlayout";
+
+export default function CreatePost() {
+  return (
+    <Mainlayout>
+      <div className="p-6">
+        <h1 className="text-2xl font-bold">Create Post</h1>
+      </div>
+    </Mainlayout>
+  );
+}
